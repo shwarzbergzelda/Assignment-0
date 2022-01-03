e6eb21fbdd89b326c45edcb20beed758429473b7
Zelda <zeldashw@gmail.com>
2022-01-03 16:11:17 -0500

    test-17 passed

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,13 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  const set = new Set();
+
+  for (const arg of args)  {
+    for (const val of arg) {
+      set.add(val);
+    }
+  }
+
+  return set;
 }
 
 // Do not edit this line;
