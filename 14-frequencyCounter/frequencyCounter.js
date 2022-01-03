beddab55ea696e81ce7fbb9b9844edb8a0adeee1
Zelda <zeldashw@gmail.com>
2022-01-03 16:05:57 -0500

    test-14 passed

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,17 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  let map = new Object();
+  
+  for (let i = 0; i < word.length; i++) {
+    let currLetter = word.substring(i, i + 1);
+    if (!map.hasOwnProperty(currLetter)) {
+      map[currLetter] = 1;
+    } else {
+      let currLetterCount = map[currLetter] + 1;
+      map[currLetter] = currLetterCount;
+    }
+  }
+
+  return map;
 }
 
 // Do not edit this line;
