87a073e3ff6f429448aa08db62b6d6212798a116
Zelda <zeldashw@gmail.com>
2022-01-03 06:33:34 -0500

    test-01 passed

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,17 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  let result = "";
+  result += title.substring(0, 1).toUpperCase();
+
+  for (let i = 1; i < title.length; i++) {
+    if (title.substring(i, i + 1) === " ") {
+      result += (" " + title.substring(i + 1, i + 2).toUpperCase());
+      i++;
+    } else {
+      result += title.substring(i, i + 1);
+    }
+  } 
+
+  return result;
 }
 
 // Do not edit this line;
