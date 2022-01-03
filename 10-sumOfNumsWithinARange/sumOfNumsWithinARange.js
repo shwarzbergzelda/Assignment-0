084f4842f7425154ce1da9609bc7892f0f161fb7
Zelda <zeldashw@gmail.com>
2022-01-03 09:28:28 -0500

    test-10 passed

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,18 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+  const startIndex = nums.indexOf(start);
+  const endIndex = nums.indexOf(end);
+  let result = 0;
+
+  if (startIndex === -1) return 0;
+  if (endIndex === -1) {
+    return nums.length - startIndex;
+  }
+
+  for (let i = startIndex; i <= endIndex; i++) {
+    result++;
+  }
+
+  return result;
 }
 
 // Do not edit this line;
