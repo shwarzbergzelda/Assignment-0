7a0571451c92edd620c527dfb0510c4275187366
Zelda <zeldashw@gmail.com>
2022-01-03 07:35:25 -0500

    test-13 passed

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
-  // Insert code here;
+  for (let i = 0; i < word.length / 2; i++) {
+    if (
+      word.substring(i, i + 1) !=
+      word.substring(word.length - 1 - i, word.length - i)
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 // Do not edit this line;
